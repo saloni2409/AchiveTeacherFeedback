1abc952c73b1a1b0fc0a79e4ca179ba839db40b9
saloni mishra <saloni@salonis-MacBook-Pro.local>
2026-03-25 16:21:25 +0530

    fix

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening on port ${port} and bound to 0.0.0.0`);
 });
